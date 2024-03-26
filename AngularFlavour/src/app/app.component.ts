7716925d5bc918c9d4b71e73b4002dc5cbde3fa5
Paridus <hxhprd05t15e07v@iisbadoni.edu.it>
2024-03-26 11:15:54 +0100

    componentiz
    adesso partono i componenti :D

--- AngularFlavour/src/app/app.component.ts
+++ AngularFlavour/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { CommonModule } from '@angular/common';
+
+// @Component({
+//   selector: 'app-test',
+//   standalone:true,
+//   imports:[TestComponent],
+//   templateUrl: './tets.component.html',
+//   styleUrl: './test.component.css'
+// })
+// export class TestComponent{
+
+// }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TestComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
