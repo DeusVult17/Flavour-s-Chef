2030375aa6ddc25b12f45521cf74e43809da6e38
Paridus_butAcasa <hxhprd05t15e507v@iisbadoni.edu.it>
2024-04-23 16:44:51 +0200

    creazione classe prenotazione
    adesso devo modificare il pom per il db

--- JavaFlavours/src/main/java/com/example/javaflavours/api/testController.java
+++ JavaFlavours/src/main/java/com/example/javaflavours/api/testController.java
@@ -1,5 +1,7 @@
 package com.example.javaflavours.api;
 
+
+import com.example.javaflavours.model.prenotazione;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,13 +24,21 @@ public class testController {
         return "shoutouts to simpleflips :D";
     }
 
+    @GetMapping("/prova")
+    public int bho(){
+        return 2;
+    }
+
     @PostMapping("/prenota")
     public Map<String,String> woo(@RequestBody Map<String,String> f){
         //String a=f.get("stringa");
         Map<String,String> response=new HashMap<>();
-        response.put("email",f.get("email"));
-        response.put("posti",f.get("posti"));
-        response.put("data",f.get("data"));
+        response.put("message","ciao");
+
+        int posti=Integer.parseInt(f.get("posti"));
+        prenotazione pr=new prenotazione(f.get("email"),f.get("data"),posti);
+        pr.inserisci();
+
         return response;
     }
 
---
+++ JavaFlavours/src/main/java/com/example/javaflavours/model/prenotazione.java
@@ -0,0 +1,64 @@
+package com.example.javaflavours.model;
+
+import java.sql.*;
+import java.util.*;
+
+public class prenotazione {
+    String email;
+    String data;
+    int posti;
+
+    public prenotazione(){
+
+    }
+
+    public prenotazione(String email, String data, int posti) {
+        this.email = email;
+        this.data = data;
+        this.posti = posti;
+    }
+
+    public void inserisci(){
+        Connection cn=null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://localhost/flavourschef","root","");
+            Statement stmt=cn.createStatement();
+            String sql="INSERT INTO prenotazione (mail,persone,data) VALUES ('"+this.email+"',"+this.posti+",'"+this.data+"')";
+            //ResultSet rs=stmt.executeQuery(sql);
+            stmt.executeUpdate(sql);
+        }catch(Exception e){
+            System.out.println("errorez");
+        }
+
+
+
+    }
+
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getPosti() {
+        return posti;
+    }
+
+    public void setPosti(int posti) {
+        this.posti = posti;
+    }
+}
